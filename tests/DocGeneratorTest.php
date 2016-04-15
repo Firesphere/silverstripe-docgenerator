1608a9b61189358e1b32ab7cc49ae698d226031b
Simon <simon@casa-laguna.net>
2016-04-15 19:47:12 +1200

    Docs and Tests added... I think

---
+++ tests/DocGeneratorTest.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Class DocGeneratorTest
+ *
+ * I'm not entirely sure how/what to test here...
+ */
+class DocGeneratorTest extends SapphireTest
+{
+
+    public function testDocGeneration()
+    {
+        $this->assertTrue(true); // Yay! Green tests!
+    }
+
+}
